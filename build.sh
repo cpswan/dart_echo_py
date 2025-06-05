52536bac9aee5672d5c4dd567c0721f9e7ff7857
Chris Swan <478926+cpswan@users.noreply.github.com>
2025-06-05 11:32:50 +0100

    feat: Multiple binaries

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 dart compile exe dart_echo_py/src/echo.dart -o dart_echo_py/bin/dart_echo_py
+dart compile exe dart_echo_py/src/showplatform.dart -o dart_echo_py/bin/dartshowplatform
 
 python -m build .
--- dart_echo_py/__init__.py
+++ dart_echo_py/__init__.py
@@ -1,5 +1,5 @@
 import os, sys, subprocess
 sys.exit(subprocess.call([
-    os.path.join(os.path.dirname(__file__), "bin/dart_echo_py"),
+    os.path.join(os.path.dirname(__file__), f"bin/{os.path.basename(sys.argv[0])}"),
     *sys.argv[1:]
 ]))
